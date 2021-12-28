336b6c0eb2ce88fcdf686cd4784f810f1bbb580f
bounderoll <bounderoll.23@gmail.com>
2021-12-28 23:32:20 +0400

    app config, template cache, base layout for template files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/bndroll/go-project-01/pkg/config"
 	"github.com/bndroll/go-project-01/pkg/handlers"
+	"github.com/bndroll/go-project-01/pkg/render"
+	"log"
 	"net/http"
 )
 
 const portNumber = ":8000"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("can't create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,74 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+var functions = template.FuncMap{}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	/* TODO get the template cache from the app config */
+
+	tc, err := CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
 
 	if err != nil {
-		fmt.Println("error parsing template:", err)
+		fmt.Println("error writing template to browser", err)
 	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return cache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+		if err != nil {
+			return cache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+
+		if err != nil {
+			return cache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+
+			if err != nil {
+				return cache, err
+			}
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
